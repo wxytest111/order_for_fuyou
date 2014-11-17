64a3af20e952c3de3791a5f33f977c88984e091f
Xinyu <xywang@thoughtworks.com>
2014-11-17 09:34:56 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderForFuyou::Application.config.secret_token = '69be8d9601764e2ff8d650090c0ad529b1a80cb58d4a8b18fbd6e5aede49c5ed146b643b6e56acac30556697e13bd572380abb17dffaa2e47ed3560639ae9d86'
